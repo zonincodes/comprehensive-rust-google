f220bbfad4f472d99709f7f586d6129f20a8287a
Zonin <business.kariukimartin@gmail.com>
2024-02-06 14:44:49 +0300

    day one

---
+++ src/Elevator.rs
@@ -0,0 +1,77 @@
+#[derive(Debug)]
+/// An event in the elevator system that the controller must react to.
+enum Event {
+    // TODO: add required variants
+    ButtonPressed(Button),
+
+    // The car has arrived at the given Floor
+    CarArrived(Floor),
+
+    // Car's door jave Opened.
+    CarDoorOpened,
+
+
+    // The car's doors have Closed.
+    CarDoorClosed,
+}
+
+type Floor = i32;
+
+/// A direction of travel.
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+}
+
+
+#[derive(Debug)]
+// A user - accessible button.
+enum Button{
+    // A button in the elevator lobby on the given floor
+    LobbyCall(Direction, Floor),
+
+
+    // A floor button within the car.
+    CarFloor(Floor),
+}
+
+/// The car has arrived on the given floor.
+fn car_arrived(floor: i32) -> Event {
+    Event::CarArrived(floor)
+}
+
+/// The car doors have opened.
+fn car_door_opened() -> Event {
+    Event::CarDoorOpened
+}
+
+/// The car doors have closed.
+fn car_door_closed() -> Event {
+    Event::CarDoorClosed
+}
+
+/// A directional button was pressed in an elevator lobby on the given floor.
+fn lobby_call_button_pressed(floor: i32, dir: Direction) -> Event {
+    Event::ButtonPressed(Button::LobbyCall(dir, floor))
+}
+
+/// A floor button was pressed in the elevator car.
+fn car_floor_button_pressed(floor: i32) -> Event {
+    Event::ButtonPressed(Button::CarFloor(floor))
+}
+
+fn main() {
+    println!(
+        "A ground floor passenger has pressed the up button: {:?}",
+        lobby_call_button_pressed(0, Direction::Up)
+    );
+    println!("The car has arrived on the ground floor: {:?}", car_arrived(0));
+    println!("The car door opened: {:?}", car_door_opened());
+    println!(
+        "A passenger has pressed the 3rd floor button: {:?}",
+        car_floor_button_pressed(3)
+    );
+    println!("The car door closed: {:?}", car_door_closed());
+    println!("The car has arrived on the 3rd floor: {:?}", car_arrived(3));
+}
