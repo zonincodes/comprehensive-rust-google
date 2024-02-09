1f3c55b6af0d49bda7f69b03aa2020c4f8794d37
Zonin <business.kariukimartin@gmail.com>
2024-02-09 12:50:03 +0300

    Enums and pattern Matching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,64 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
 
+#[derive(Debug)]
+enum usState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quater(usState),
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i +1)
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quater(usState::Alabama) => 25,
+        _ => 0,
+    }
+}
 fn main() {
-    
+    let home: IpAddr= IpAddr::V4(String::from("127.0.0.1"));
+    let loopback: IpAddr = IpAddr::V6(String::from("::1"));
+
+    println!("{:?} {:?}", home, loopback);
+    println!("Penny is worth: ${}", value_in_cents(Coin::Penny));
+
+    let five: Option<i32> = Some(5);
+    // let six = plus_one(five);
+    // let none = plus_one(None);
+
+    println!("{:?}", five);
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to  be {}", max);
+    }
+
+    let mut count = 0;
+    let coin = Coin::Quater(usState::Alabama);
+    if let Coin::Quater(state) = coin {
+        println!("State quater from {:?}!", state);
+    } else {
+        count  +=  1;
+    }
 }
